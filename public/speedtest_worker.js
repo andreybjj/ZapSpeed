ca85cf200901327a6b1d5ec6689513fb459da4e2
Andrey Dev <andreysamaniego1@gmail.com>
2026-05-20 20:25:30 -0400

    feat: add LibreSpeed worker loader

---
+++ public/speedtest_worker.js
@@ -0,0 +1,2 @@
+/* Loads the official LibreSpeed worker. The measurement engine is not reimplemented here. */
+importScripts("https://cdn.jsdelivr.net/gh/librespeed/speedtest@master/speedtest_worker.js");
